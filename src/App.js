3e7ed4d5eefec6c0051505dd8572879aee0149c4
FoxcroftN <neilfoxcroftpro@gmail.com>
2020-11-14 13:41:52 +0200

    Change the Git repo root folder
    Remove the outer hidden git file in parent folder.
    Re-Init git in the Code local folder
    And Push to new remote origin

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import axios from 'axios'; 
+import React,{Component} from 'react';
+import './App.css';
+
+import { Button, Input } from '@material-ui/core'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import FileSelectIcon from '@material-ui/icons/Folder';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+class App extends Component { 
+  
+    state = { 
+  
+      // Initially, no file is selected 
+      selectedFile: null
+    }; 
+     
+    // On file select (from the pop up) 
+    onFileChange = event => { 
+     
+      // Update the state 
+      this.setState({ selectedFile: event.target.files[0] }); 
+     
+    }; 
+     
+    // On file upload (click the upload button) 
+    onFileUpload = () => { 
+     
+      // Create an object of formData 
+      const formData = new FormData(); 
+     
+      // Update the formData object 
+      if(this.state.selectedFile) {
+        formData.append( 
+          "myFile", 
+          this.state.selectedFile, 
+          this.state.selectedFile.name 
+        ); 
+      }
+      else {
+        alert('No File selected! Please choose your file.');
+      } 
+     
+      // Details of the uploaded file 
+      console.log(this.state.selectedFile); 
+     
+      // Request made to the backend api 
+      // Send formData object 
+      axios.post("api/uploadfile", formData); 
+    }; 
+     
+    // File content to be displayed after 
+    // file upload is complete 
+    fileData = () => { 
+     
+      if (this.state.selectedFile) { 
+          
+        return ( 
+          <div id="text-info"> 
+            <h2>File Details:</h2> 
+            <p>File Name: {this.state.selectedFile.name}</p> 
+            <p>File Type: {this.state.selectedFile.type}</p> 
+            <p> 
+              Last Modified:{" "} 
+              {this.state.selectedFile.lastModifiedDate.toDateString()} 
+            </p> 
+          </div> 
+        ); 
+      } else { 
+        return ( 
+          <div id="text-no-info"> 
+            <br /> 
+            <h4 id="heading-4">Select a file before uploading!</h4> 
+          </div> 
+        ); 
+      } 
+    }; 
+     
+    render() { 
+     
+      return ( 
+        <div> 
+            <h1 id="heading-1"> 
+              Neil Foxcroft - POPI DataCapture 
+            </h1> 
+            <h3 id="heading-3"> 
+              File Upload using to identify and caputure sensitive personal data. 
+            </h3>
+
+            <div id="input-area">
+              <div>
+                  <Input
+                    id="input-btn"
+                    endAdornment={<FileSelectIcon/>}
+                    type="file" 
+                    onChange={this.onFileChange} /> 
+                  <Button 
+                    id="upload-button"
+                    onClick={this.onFileUpload}
+                    variant="contained"
+                    color="default"
+                    startIcon={<CloudUploadIcon/>} > 
+                    Upload! 
+                  </Button>
+              </div>
+            </div> 
+          {this.fileData()} 
+        </div> 
+      ); 
+    } 
+  } 
+  
+  export default App;
